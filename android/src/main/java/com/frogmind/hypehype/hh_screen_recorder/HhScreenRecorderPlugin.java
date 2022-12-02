febd79746a15b626523147e3fe9fd0ed9bbd28ca
Inan Evin <inanevin@gmail.com>
2022-12-02 14:26:22 +0200

    up

--- android/src/main/java/com/frogmind/hypehype/hh_screen_recorder/HhScreenRecorderPlugin.java
+++ android/src/main/java/com/frogmind/hypehype/hh_screen_recorder/HhScreenRecorderPlugin.java
@@ -26,6 +26,11 @@ public class HhScreenRecorderPlugin implements FlutterPlugin, MethodCallHandler 
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
+    }
+    if(call.method.equals("startRecording"))
+    {
+      result.success("Inan Android: startRecording");
+      System.out.println("Inan android: start recording");
     } else {
       result.notImplemented();
     }
--- lib/hh_screen_recorder.dart
+++ lib/hh_screen_recorder.dart
@@ -1,8 +1,20 @@
+import 'dart:developer';
 
 import 'hh_screen_recorder_platform_interface.dart';
+import 'package:flutter/services.dart';
+
 
 class HhScreenRecorder {
+
+    static const MethodChannel _channel = MethodChannel('hh_screen_recorder');
+
   Future<String?> getPlatformVersion() {
     return HhScreenRecorderPlatform.instance.getPlatformVersion();
+  }
+
+  void startRecording({required String filename, String? directory}) async
+  {
+    var response = await _channel.invokeMethod('startRecording',{ "filename": filename, "directory": directory});
+    log("Got response!");
   }
 }
