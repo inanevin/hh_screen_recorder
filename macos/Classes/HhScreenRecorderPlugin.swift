3f1d2720440e81fd532f8b27439c271e70fe4ccd
Inan Evin <inanevin@gmail.com>
2022-12-15 12:22:07 +0200

    sync

--- macos/Classes/HhScreenRecorderPlugin.swift
+++ macos/Classes/HhScreenRecorderPlugin.swift
@@ -17,71 +17,74 @@ public class HhScreenRecorderPlugin: NSObject, FlutterPlugin,
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
       flutterRes = result
         
-      if (call.method == "startRecording")
-      {
-          print("HHRecorder: Start Recording")
-         
-          RPScreenRecorder.shared().startRecording { err in
-            guard err == nil else {
-                print("HHRecorder: Error starting recording: \(err.debugDescription)")
-                result(false)
-                return }
-              
-              print("HHRecorder: Started recording.")
-              result(true)
-          }
+      if #available(OSX 11.0, *) {
           
-      }
-      else if (call.method == "stopRecording")
-      {
-          print("HHRecorder: Attempting to stop recording & show preview window")
-
-          RPScreenRecorder.shared().stopRecording { preview, err in
-            guard let preview = preview else {
-                print("HHRecorder: Error stopping recording: no preview window!");
-                result(false)
-                return
-            }
-              
-              if let err = err {
-                  print("HHRecorder: Error stopping recording: \(err.localizedDescription)")
-                  result(false)
-                  return
+          if (call.method == "startRecording")
+          {
+              print("HHRecorder: Start Recording")
+             
+              RPScreenRecorder.shared().startRecording { err in
+                guard err == nil else {
+                    print("HHRecorder: Error starting recording: \(err.debugDescription)")
+                    result(false)
+                    return }
+                  
+                  print("HHRecorder: Started recording.")
+                  result(true)
               }
               
-              if #available(OSX 11.0, *) {
+          }
+          else if (call.method == "stopRecording")
+          {
+              print("HHRecorder: Attempting to stop recording & show preview window")
+
+              RPScreenRecorder.shared().stopRecording { preview, err in
+                guard let preview = preview else {
+                    print("HHRecorder: Error stopping recording: no preview window!");
+                    result(false)
+                    return
+                }
+                  
+                  if let err = err {
+                      print("HHRecorder: Error stopping recording: \(err.localizedDescription)")
+                      result(false)
+                      return
+                  }
+                  
                   preview.modalPresentationStyle = .overFullScreen
                   preview.previewControllerDelegate = self
                     UIApplication.shared.delegate?.window??.rootViewController?.present(preview, animated: true)
-                  
-              } else {
-                  // Fallback on earlier versions
+                
               }
-            
+              
+          }
+          else if (call.method == "pauseRecording")
+          {
+              result(false)
+          }
+          else if (call.method == "resumeRecording")
+          {
+              result(false)
+          }
+          else if (call.method == "isPauseResumeEnabled")
+          {
+              result(false)
+          }
+          else if (call.method == "isRecordingSupported")
+          {
+              // iOS 9.0+ is always supported on HH
+              result(true)
+          }
+          else
+          {
+              result(FlutterMethodNotImplemented)
           }
           
-      }
-      else if (call.method == "pauseRecording")
-      {
+      } else {
+          print("HHRecorder: ReplayKit is only availab on MacOS 11+")
           result(false)
       }
-      else if (call.method == "resumeRecording")
-      {
-          result(false)
-      }
-      else if (call.method == "isPauseResumeEnabled")
-      {
-          result(false)
-      }
-      else if (call.method == "isRecordingSupported")
-      {
-          // iOS 9.0+ is always supported on HH
-          result(true)
-      }
-      else
-      {
-          result(FlutterMethodNotImplemented)
-      }
+     
   }
     
     @available(OSX 11.0, *)
