04bf85a78d00a3964d0d5e10a768fa3cb4bd2622
Inan Evin <inanevin@gmail.com>
2022-12-16 14:21:58 +0300

    sync

--- lib/hh_screen_recorder.dart
+++ lib/hh_screen_recorder.dart
@@ -46,42 +46,77 @@ class HhScreenRecorder {
 
   Future<bool> startRecording(
       {required String filename, String? foldername, bool? recordAudio}) async {
-    var response = await _channel.invokeMethod('startRecording', {
-      "filename": filename,
-      "foldername": foldername,
-      "recordAudio": recordAudio
-    });
-    return response;
+     try{
+        var response = await _channel.invokeMethod('startRecording', {
+          "filename": filename,
+          "foldername": foldername,
+          "recordAudio": recordAudio
+        });
+        return response;
+    }
+    on Exception catch(ex)
+    {
+      throw Exception(ex.toString());
+    }
   }
 
   Future<bool> stopRecording() async {
-    var response = await _channel.invokeMethod('stopRecording');
-    return response;
+     try{
+       var response = await _channel.invokeMethod('stopRecording');
+      return response;
+    }
+    on Exception catch(ex)
+    {
+      throw Exception(ex.toString());
+    }
   }
 
   Future<bool> pauseRecording() async {
-    var response = await _channel.invokeMethod('pauseRecording');
-    return response;
+     try{
+      var response = await _channel.invokeMethod('pauseRecording');
+      return response;
+    }
+    on Exception catch(ex)
+    {
+      throw Exception(ex.toString());
+    }
   }
 
   Future<bool> resumeRecording() async {
-    var response = await _channel.invokeMethod('resumeRecording');
-    return response;
+    try{
+      var response = await _channel.invokeMethod('resumeRecording');
+      return response;
+    }
+    on Exception catch(ex)
+    {
+      throw Exception(ex.toString());
+    }
   }
 
   // Not all Android API's support MediaRecorder stop/pause
   // Do not let the user pause if so.
   // iOS Replay Kit does not support this at all atm.
   Future<bool> isPauseResumeEnabled() async {
-    var response = await _channel.invokeMethod('isPauseResumeEnabled');
-    return response;
+    try{
+      var response = await _channel.invokeMethod('isPauseResumeEnabled');
+      return response;
+    }
+    on Exception catch(ex)
+    {
+      throw Exception(ex.toString());
+    }
   }
 
   // Should check for device information,
   // Whether h264 encoder & MP4 file format is supported or not.
   // True for all iOS & macOS devices HH supports.
   Future<bool> isRecordingSupported() async {
-    var response = await _channel.invokeMethod('isRecordingSupported');
-    return response;
+    try{
+      var response = await _channel.invokeMethod('isRecordingSupported');
+      return response;
+    }
+    on Exception catch (ex) {
+      throw Exception(ex.toString());
+    }
   }
 }
